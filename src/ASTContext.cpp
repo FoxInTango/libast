7c6b006a0c58f2d730eb9cb12f73459df4c3c58b
Jack.WSL <foxintango@yeah.net>
2023-06-22 19:12:43 +0800

    alpine

--- src/ASTContext.cpp
+++ src/ASTContext.cpp
@@ -4,16 +4,6 @@ using namespace foxintango;
 ASTContext::ASTContext(){}
 ASTContext::~ASTContext(){}
 
-Error ASTContext::load(const String& path){
-    ASTDocument* document = new ASTDocument();
-
-    if(document){
-        return document->load(path);
-    }
-
-    return 1;
-}
-
 Error ASTContext::execute(){
     for(Index index = 0;index < subelements.size();index ++){
         if(subelements[index]) subelements[index]->execute(this,0);
