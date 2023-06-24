0600f94ed1623caf84afe6a18333d5932d326df4
Jack.WSL <foxintango@yeah.net>
2023-06-24 17:25:35 +0800

    alpine

--- src/ASTDocument.cpp
+++ src/ASTDocument.cpp
@@ -5,16 +5,3 @@ using namespace foxintango;
 ASTDocument::ASTDocument() {}
 ASTDocument::ASTDocument(const URL& url) {}
 ASTDocument::~ASTDocument() {}
-
-Error ASTDocument::load(const URL& url){
-    Stream stream(url);
-    /** 判断语言类型 调取相应语言处理
-     */
-    Index index = 0;
-    while(stream.readable()){
-        switch(stream[index]){
-        
-        default:break;
-        }
-    }
-}
