8bb1ab36ac2c138516badda3de28932ab4ae81ad
sparrow <sparrow@wsl.me>
2025-08-31 23:33:35 +0800

    Sun Aug 31 23:33:33 CST 2025

---
+++ .make/depend.dir/libmodule.mk
@@ -0,0 +1,12 @@
+DEPEND_TARGETS += libmodule.build
+ECHO_TARGETS += libmodule.echo
+libmodule.build:
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/ && make && make install
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+libmodule.echo:
+	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/ && make echo
+	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
