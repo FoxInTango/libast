3add1157d5d0ce211a46f36ab35be3b6626df734
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 17:59:24 +0800

    Sun Sep  7 05:59:24 PM CST 2025

--- .make/depend.dir/libmodule.mk
+++ .make/depend.dir/libmodule.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmodule.recursive
 ECHO_TARGETS += libmodule.echo
 libmodule.recursive:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/ && make recursive && make install
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libmodule/.make/super
+	cd /home/lidali/alpine/libraries/libmodule/ && make recursive && make install
+	-rm /home/lidali/alpine/libraries/libmodule/.make/super
 libmodule.echo:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libast/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/ && make echo
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libmodule/.make/super
+	cd /home/lidali/alpine/libraries/libmodule/ && make echo
+	-rm /home/lidali/alpine/libraries/libmodule/.make/super
