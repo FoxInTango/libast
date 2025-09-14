6bbc6a81db29d011961aa5e62f52f87ca9c42f2c
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:21 +0800

    Mon Sep 15 07:35:21 AM CST 2025

--- .make/depend.dir/libecho.mk
+++ .make/depend.dir/libecho.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libecho.recursive
 ECHO_TARGETS += libecho.echo
 libecho.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libecho/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libecho/.make/super
-	cd /home/lidali/alpine/libraries/libecho/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libast/               >> /volumes/llama/home/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libast/.make >> /volumes/llama/home/alpine/libraries/libecho/.make/super
+	cd /volumes/llama/home/alpine/libraries/libecho/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libecho/.make/super
 libecho.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libecho/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libecho/.make/super
-	cd /home/lidali/alpine/libraries/libecho/ && make echo
-	-rm /home/lidali/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libast/               >> /volumes/llama/home/alpine/libraries/libecho/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libast/.make >> /volumes/llama/home/alpine/libraries/libecho/.make/super
+	cd /volumes/llama/home/alpine/libraries/libecho/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libecho/.make/super
--- .make/depend.dir/libmodule.mk
+++ .make/depend.dir/libmodule.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libmodule.recursive
 ECHO_TARGETS += libmodule.echo
 libmodule.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libmodule/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libmodule/.make/super
-	cd /home/lidali/alpine/libraries/libmodule/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libast/               >> /volumes/llama/home/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libast/.make >> /volumes/llama/home/alpine/libraries/libmodule/.make/super
+	cd /volumes/llama/home/alpine/libraries/libmodule/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libmodule/.make/super
 libmodule.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libast/               >> /home/lidali/alpine/libraries/libmodule/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libast/.make >> /home/lidali/alpine/libraries/libmodule/.make/super
-	cd /home/lidali/alpine/libraries/libmodule/ && make echo
-	-rm /home/lidali/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libast/               >> /volumes/llama/home/alpine/libraries/libmodule/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libast/.make >> /volumes/llama/home/alpine/libraries/libmodule/.make/super
+	cd /volumes/llama/home/alpine/libraries/libmodule/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libmodule/.make/super
